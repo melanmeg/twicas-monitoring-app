2986de8d2d9f094274bfc93f389ed9d75074b3dc
melanmeg <naoya05280708@gmail.com>
2025-01-13 22:49:22 +0900

    fix

--- containers/publisher/src/environments.ts
+++ containers/publisher/src/environments.ts
@@ -1,22 +1,13 @@
 import "./dotenv.js";
 
 export class Config {
-  static readonly YOUTUBE_TEST_URL: string = process.env.YOUTUBE_TEST_URL!;
+  static readonly RABBITMQ_URL: string = process.env.RABBITMQ_URL!;
 
-  static readonly OPENSEARCH_URL: string = process.env.OPENSEARCH_URL!;
+  static readonly RABBITMQ_PORT: string = process.env.RABBITMQ_PORT!;
 
-  static readonly OPENSEARCH_USER: string = process.env.OPENSEARCH_USER!;
+  static readonly RABBITMQ_USER: string = process.env.RABBITMQ_USER!;
 
-  static readonly OPENSEARCH_PASSWORD: string =
-    process.env.OPENSEARCH_PASSWORD!;
-
-  static readonly USER_AGENT: string = process.env.USER_AGENT!;
-
-  static readonly DOWNLOAD_DIR: string = process.env.DOWNLOAD_DIR!;
+  static readonly RABBITMQ_PASSWORD: string = process.env.RABBITMQ_PASSWORD!;
 
   static readonly PORT_NUM: number = parseInt(process.env.PORT_NUM!, 10);
-
-  static readonly LOG_DIR: string = process.env.LOG_DIR!;
-
-  static readonly LOG_FILE_PATH: string = `${Config.LOG_DIR}/webhook.log`;
 }
